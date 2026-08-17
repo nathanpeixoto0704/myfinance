4d2c34f94715603c4264b94aec9266be9349dfe2
Nathan Berbert Peixoto <nathan.berbertpeixoto@gmail.com>
2026-08-17 14:57:27 +0000

    ajuste do SUPABASE_URL

--- config.js
+++ config.js
@@ -9,6 +9,6 @@
 // 4. Troque TABLE_NAME se sua tabela tiver outro nome
 // ============================================================
 
-const SUPABASE_URL = "https://yhuttwgufaiyzzeijkpx.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://yhuttwgufaiyzzeijkpx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlodXR0d2d1ZmFpeXp6ZWlqa3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NTg2NjYsImV4cCI6MjEwMTQzNDY2Nn0.4jasxJw0wKz1za4ydoMTeq27HZw2WBqHzDUusUgK_LY";
 const TABLE_NAME = "movimentacoes";
